ac0323dc6a17d9d9a10a974e7cfda8206d599800
silverstar194 <maloney4@wisc.edu>
2020-04-09 12:46:37 -0700

    added changes

---
+++ backend/token_api/models/application.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from .action import Action
+
+class Device(models.Model):
+    device_id = models.CharField(max_length=64)
+
+    application = models.CharField(max_length=64)
+
+    actions = models.ManyToManyField(Action, related_name="action")
+
+    class Meta:
+        unique_together = [['device_id', 'application']]
+
+    def __str__(self):
+        return "Device {0}".format(self.device_id)
+
+    def natural_key(self):
+        (self.device_id, )
